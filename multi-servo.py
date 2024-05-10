4aada86a8e888c7287612c97f10abef95a8e2f9c
max-hans <m.hans@me.com>
2024-05-10 18:01:32 +0200

    add seutp and multi-servo

--- multi-servo.py
+++ multi-servo.py
@@ -2,11 +2,14 @@ import time
 from library import Servo
 
 servo1 = Servo(pin=28)  #To be changed according to the pin used
+servo2 = Servo(pin=27)  #To be changed according to the pin used
 
 while True:
     print(0)
     servo1.move(0)  # turns the servo to 0°.
+    servo2.move(0)  # turns the servo to 0°.
     time.sleep(0.5)
     print(1)
     servo1.move(90)  # turns the servo to 90°.
+    servo2.move(90)  # turns the servo to 90°.
     time.sleep(0.5)
